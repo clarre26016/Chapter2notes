907f8674e6d61b66e266ff945dcd14986e426f0f
spbx <spbx@frensou.org>
2023-02-21 14:19:15 -0800

    adding whileLoops1

---
+++ whileLoops1.js
@@ -0,0 +1,31 @@
+const readLine = require('readline-sync');
+
+let w = 0;
+while (w < 26){
+    console.log(w);
+    w++
+}
+
+let c = 0;
+while (c < 51){
+    console.log(c);
+   c++
+}
+
+let s = -10;
+while (s < 11){
+    console.log(s);
+    s++
+}
+
+let m = 0;
+while (m > -51) {
+    console.log(m);
+    m--
+}
+let l = 0;
+while (l < 100) {
+    if (l % 2 == 0) {
+        console.log(c);
+    }
+}
